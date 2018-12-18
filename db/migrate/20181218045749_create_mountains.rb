6f2863ac233b56c864be304305274777e01688ef
MacInnes <califormula@gmail.com>
2018-12-17 22:03:28 -0700

    Corrects int to integer in migration for Mountains

--- db/migrate/20181218045749_create_mountains.rb
+++ db/migrate/20181218045749_create_mountains.rb
@@ -2,7 +2,7 @@ class CreateMountains < ActiveRecord::Migration[5.2]
   def change
     create_table :mountains do |t|
       t.string :name
-      t.int :altitude
+      t.integer :altitude
       t.string :summit
       t.string :difficulty
       t.string :range
