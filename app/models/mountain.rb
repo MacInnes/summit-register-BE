ac5991a79c2f3a39fe5f5abed5a9e66dd6b680d0
andrewetobin <andrewetobin@gmail.com>
2018-12-18 12:47:37 -0700

    Adds tests for Mountain model

--- app/models/mountain.rb
+++ app/models/mountain.rb
@@ -1,3 +1,4 @@
 class Mountain < ApplicationRecord
-  attr_reader :name, :altitude, :summit, :difficulty, :range
+  validates_presence_of :name, :altitude, :summit, :difficulty, :range
+
 end
---
+++ spec/models/mountain_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+describe Mountain, type: :model do
+  before(:each) do
+    @elbert = Mountain.create(
+      name: 'Mt. Elbert',
+      altitude: 14433,
+      summit: "39.118075,-106.445417",
+      difficulty: "Blue",
+      range: "Sawatch"
+    )
+    end
+  describe "it exists" do
+    it "exists" do
+
+      expect(@elbert).to be_a(Mountain)
+    end
+  end
+  describe "attributes" do
+    it "has attributes" do
+
+      expect(@elbert.name).to eq('Mt. Elbert')
+      expect(@elbert.altitude).to eq(14433)
+      expect(@elbert.summit).to eq("39.118075,-106.445417")
+      expect(@elbert.difficulty).to eq('Blue')
+      expect(@elbert.range).to eq('Sawatch')
+    end
+  end
+  describe 'Validations' do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :altitude }
+    it { should validate_presence_of :summit }
+    it { shouldgit  validate_presence_of :difficulty }
+    it { should validate_presence_of :range }
+  end
+end
