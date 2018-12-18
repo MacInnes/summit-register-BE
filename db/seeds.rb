482681a79db586ea856c7fd41b310713637c454a
andrewetobin <andrewetobin@gmail.com>
2018-12-17 20:55:08 -0700

    adds seeds file for all 14ers and three test locations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,431 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Mountain.create(
+  name: 'Mt. Elbert',
+  altitude: 14433,
+  summit: [39.118075, -106.445417],
+  difficulty: "Blue",
+  range: "Sawatch"
+)
+Mountain.create(
+  name: 'Mt. Massive',
+  altitude: 14421,
+  summit: [39.187298, -106.475548],
+  difficulty: "Blue",
+  range: "Sawatch"
+)
+Mountain.create(
+  name: 'Mt. Harvard',
+  altitude: 14420,
+  summit: [38.924328, -106.320618],
+  difficulty: "Blue",
+  range: "Sawatch"
+)
+Mountain.create(
+  name: 'Blanca Peak',
+  altitude: 14345,
+  summit: [37.577473, -105.485443],
+  difficulty: "Black",
+  range: "Sangre de Cristo"
+)
+Mountain.create(
+  name: 'La Plata Peak',
+  altitude: 14336,
+  summit: [39.029251, -106.473145],
+  difficulty: "Blue",
+  range: "Sawatch"
+)
+Mountain.create(
+  name: 'Uncompahgre Peak',
+  altitude: 14309,
+  summit: [38.071507, -107.462166],
+  difficulty: "Blue",
+  range: "San Juan"
+)
+Mountain.create(
+  name: 'Crestone Peak',
+  altitude: 14294,
+  summit: [37.966972, -105.585304],
+  difficulty: "Black",
+  range: "Sangre de Cristo"
+)
+Mountain.create(
+  name: 'Mt. Lincoln',
+  altitude: 14286,
+  summit: [39.351391, -106.111404],
+  difficulty: "Green",
+  range: "Mosquito"
+)
+Mountain.create(
+  name: 'Grays Peak',
+  altitude: 14270,
+  summit: [39.633820, -105.817520],
+  difficulty: "Green",
+  range: "Front"
+)
+Mountain.create(
+  name: 'Mt. Antero',
+  altitude: 14269,
+  summit: [38.674088, -106.246201],
+  difficulty: "Green",
+  range: "Sawatch"
+)
+Mountain.create(
+  name: 'Torreys Peak',
+  altitude: 14267,
+  summit: [39.642742, -105.821259],
+  difficulty: "Green",
+  range: "Front"
+)
+Mountain.create(
+  name: 'Castle Peak',
+  altitude: 14265,
+  summit: [39.009647, -106.86144],
+  difficulty: "Blue",
+  range: "Elk"
+)
+Mountain.create(
+  name: 'Quandary Peak',
+  altitude: 14265,
+  summit: [39.397236, -106.106430],
+  difficulty: "Green",
+  range: "Tenmile"
+)
+Mountain.create(
+  name: 'Mt. Evans',
+  altitude: 14264,
+  summit: [39.588360, -105.643333],
+  difficulty: "Blue",
+  range: "Front"
+)
+Mountain.create(
+  name: 'Longs Peak',
+  altitude: 14255,
+  summit: [40.254902, -105.615738],
+  difficulty: "Double Black",
+  range: "Front"
+)
+Mountain.create(
+  name: 'Mt. Wilson',
+  altitude: 14246,
+  summit: [37.839310, -107.991570],
+  difficulty: "Black",
+  range: "San Juan"
+)
+Mountain.create(
+  name: 'Mt. Shavano',
+  altitude: 14229,
+  summit: [38.619083, -106.239296],
+  difficulty: "Green",
+  range: "Sawatch"
+)
+Mountain.create(
+  name: 'Mt. Princeton',
+  altitude: 14197,
+  summit: [38.749062, -106.242432],
+  difficulty: "Green",
+  range: "Sawatch"
+)
+Mountain.create(
+  name: 'Mt. Belford',
+  altitude: 14197,
+  summit: [38.960575, -106.360832],
+  difficulty: "Blue",
+  range: "Sawatch"
+)
+Mountain.create(
+  name: 'Crestone Needle',
+  altitude: 14197,
+  summit: [37.964737, -105.576683],
+  difficulty: "Black",
+  range: "Sangre de Cristo"
+)
+Mountain.create(
+  name: 'Mt. Yale',
+  altitude: 14196,
+  summit: [38.844051, -106.313965],
+  difficulty: "Blue",
+  range: "Sawatch"
+)
+Mountain.create(
+  name: 'Mt. Bross',
+  altitude: 14172,
+  summit: [39.335060, -106.107376],
+  difficulty: "Green",
+  range: "Mosquito"
+)
+Mountain.create(
+  name: 'Kit Carson',
+  altitude: 14165,
+  summit: [37.979759, -105.602562],
+  difficulty: "Blue",
+  range: "Sangre de Cristo"
+)
+Mountain.create(
+  name: 'Maroon Peak',
+  altitude: 14156,
+  summit: [39.070713, -106.989113],
+  difficulty: "Double Black",
+  range: "Elk"
+)
+Mountain.create(
+  name: 'Tabeguache Peak',
+  altitude: 14155,
+  summit: [38.625622, -106.250710],
+  difficulty: "Blue",
+  range: "Sawatch"
+)
+Mountain.create(
+  name: 'Mt. Oxford',
+  altitude: 14153,
+  summit: [38.964680, -106.338432],
+  difficulty: "Blue",
+  range: "Sawatch"
+)
+Mountain.create(
+  name: 'Mt. Sneffels',
+  altitude: 14150,
+  summit: [38.003605, -107.792229],
+  difficulty: "Black",
+  range: "San Juan"
+)
+Mountain.create(
+  name: 'Mt. Democrat',
+  altitude: 14148,
+  summit: [39.339542, -106.139946],
+  difficulty: "Green",
+  range: "Mosquito"
+)
+Mountain.create(
+  name: 'Capital Peak',
+  altitude: 14130,
+  summit: [39.150166, -107.083221],
+  difficulty: "Double Black",
+  range: "Elk"
+)
+Mountain.create(
+  name: 'Pikes Peak',
+  altitude: 14110,
+  summit: [38.840542, -105.044357],
+  difficulty: "Blue",
+  range: "Front"
+)
+Mountain.create(
+  name: 'Snowmass Mountain',
+  altitude: 14092,
+  summit: [39.118809, -107.066528],
+  difficulty: "Black",
+  range: "Elk"
+)
+Mountain.create(
+  name: 'Windom Peak',
+  altitude: 14087,
+  summit: [37.621235, -107.591774],
+  difficulty: "Black",
+  range: "San Juan"
+)
+Mountain.create(
+  name: 'Mt. Eolus',
+  altitude: 14083,
+  summit: [37.621948, -107.622498],
+  difficulty: "Black",
+  range: "San Juan"
+)
+Mountain.create(
+  name: 'Challenger Point',
+  altitude: 14081,
+  summit: [37.980267, -105.606766],
+  difficulty: "Blue",
+  range: "Sangre de Cristo"
+)
+Mountain.create(
+  name: 'Mt. Columbia',
+  altitude: 14073,
+  summit: [38.903957, -106.297485],
+  difficulty: "Blue",
+  range: "Sawatch"
+)
+Mountain.create(
+  name: 'Missouri Mountain',
+  altitude: 14067,
+  summit: [38.947559, -106.378471],
+  difficulty: "Blue",
+  range: "Sawatch"
+)
+Mountain.create(
+  name: 'Humboldt Peak',
+  altitude: 14064,
+  summit: [37.976105, -105.555351],
+  difficulty: "Blue",
+  range: "Sanre de Cristo"
+)
+Mountain.create(
+  name: 'Mt. Bierstadt',
+  altitude: 14060,
+  summit: [39.582638, -105.668610],
+  difficulty: "Blue",
+  range: "Front"
+)
+Mountain.create(
+  name: 'Sunlight Peak',
+  altitude: 14059,
+  summit: [37.627285, -107.595894],
+  difficulty: "Black",
+  range: "San Juan"
+)
+Mountain.create(
+  name: 'Handies Peak',
+  altitude: 14048,
+  summit: [37.913021, -107.504478],
+  difficulty: "Blue",
+  range: "San Juan"
+)
+Mountain.create(
+  name: 'Culebra Peak',
+  altitude: 14047,
+  summit: [37.122173, -105.185593],
+  difficulty: "Blue",
+  range: "Sangre de Cristo"
+)
+Mountain.create(
+  name: 'Mt. Lindsey',
+  altitude: 14042,
+  summit: [37.583801, -105.444763],
+  difficulty: "Blue",
+  range: "Sangre de Cristo"
+)
+Mountain.create(
+  name: 'Ellingwood Point',
+  altitude: 14042,
+  summit: [37.582508, -105.492569],
+  difficulty: "Blue",
+  range: "Sangre de Cristo"
+)
+Mountain.create(
+  name: 'Little Bear Peak',
+  altitude: 14037,
+  summit: [37.566624, -105.497162],
+  difficulty: "Double Black",
+  range: "Sangre de Cristo"
+)
+Mountain.create(
+  name: 'Mt. Sherman',
+  altitude: 14036,
+  summit: [39.225006, -106.169945],
+  difficulty: "Green",
+  range: "Mosquito"
+)
+Mountain.create(
+  name: 'Redcloud Peak',
+  altitude: 14034,
+  summit: [37.940880, -107.421654],
+  difficulty: "Blue",
+  range: "San Juan"
+)
+Mountain.create(
+  name: 'Pyramid Peak',
+  altitude: 14018,
+  summit: [39.071545, -106.950188],
+  difficulty: "Black",
+  range: "Elk"
+)
+Mountain.create(
+  name: 'Wilson Peak',
+  altitude: 14017,
+  summit: [37.859913, -107.984795],
+  difficulty: "Black",
+  range: "San Juan"
+)
+Mountain.create(
+  name: 'Wetterhorn Peak',
+  altitude: 14015,
+  summit: [38.060593, -107.510834],
+  difficulty: "Black",
+  range: "San Juan"
+)
+Mountain.create(
+  name: 'San Luis Peak',
+  altitude: 14014,
+  summit: [37.986897, -106.931389],
+  difficulty: "Green",
+  range: "San Juan"
+)
+Mountain.create(
+  name: 'Mt. of the Holy Cross',
+  altitude: 14005,
+  summit: [39.466713, -106.481766],
+  difficulty: "Blue",
+  range: "Sawatch"
+)
+Mountain.create(
+  name: 'Huron Peak',
+  altitude: 14003,
+  summit: [38.945423, -106.438126],
+  difficulty: "Blue",
+  range: "Sawatch"
+)
+Mountain.create(
+  name: 'Sunshine Peak',
+  altitude: 14001,
+  summit: [37.922604, -107.425606],
+  difficulty: "Blue",
+  range: "San Juan"
+)
+Mountain.create(
+  name: 'Mt. Cameron',
+  altitude: 14238,
+  summit: [39.346844, -106.118576],
+  difficulty: "Green",
+  range: "Mosquito"
+)
+Mountain.create(
+  name: 'El Diente',
+  altitude: 14159,
+  summit: [37.839275, -108.005219],
+  difficulty: "Black",
+  range: "San Juan"
+)
+Mountain.create(
+  name: 'Conundrum Peak',
+  altitude: 14060,
+  summit: [39.01563, -106.86294],
+  difficulty: "Blue",
+  range: "Elk"
+)
+Mountain.create(
+  name: 'North Eolus',
+  altitude: 14039,
+  summit: [37.625416, -107.620995],
+  difficulty: "Blue",
+  range: "San Juan"
+)
+Mountain.create(
+  name: 'North Maroon',
+  altitude: 14014,
+  summit: [39.076077, -106.987267],
+  difficulty: "Black",
+  range: "Elk"
+)
+Mountain.create(
+  name: 'Mt. Sanitas',
+  altitude: 6843,
+  summit: [40.034434, -105.305301],
+  difficulty: "Blue",
+  range: "Front Range"
+)
+Mountain.create(
+  name: 'Highlands Ranch High Point',
+  altitude: 6309,
+  summit: [39.511751, -104.956245],
+  difficulty: "Green",
+  range: "Front Range"
+)
+Mountain.create(
+  name: 'Dry Peak',
+  altitude: 5207,
+  summit: [39.753114, -104.994165],
+  difficulty: "Green",
+  range: "Front"
+)
